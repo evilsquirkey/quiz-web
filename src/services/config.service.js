8a4e6233bb5c26f5e6cb825474942d888e2a6a02
sam worwood <worwood@oath.com>
2022-09-21 12:17:07 +0100

    correcting base url

--- src/services/config.service.js
+++ src/services/config.service.js
@@ -2,7 +2,7 @@ angular.module('app').factory('ConfigService', [function() {
 
     var service = {};
 
-    service.apiBase = 'https://quiz-api-sw.us-west-2.elasticbeanstalk.com/quiz';
+    service.apiBase = 'https://quiz-api-sw.us-west-2.elasticbeanstalk.com/';
 
     return service;
 }]);
